6ea1ad21ab17fc7d2ff0479db7e37b4005ddb806
Woz4tetra <woz4tetra@gmail.com>
2015-10-13 20:56:01 -0400

    Finished prompts and map makers for Jason to finish

--- ben_projects/Self-Driving Buggy Rev. 6/binder.py
+++ ben_projects/Self-Driving Buggy Rev. 6/binder.py
@@ -2,6 +2,8 @@ from board.wrapper import get_gps
 from board.wrapper import get_encoder
 from board.wrapper import get_imu
 
+from map import map_maker
+
 import math
 
 
@@ -9,27 +11,35 @@ class Binder:
     def __init__(self):
         self.longitude, self.latitude, gps_speed, \
             gps_angle, satellites, fix_quality = get_gps()
-        self.encoder_position = [self.longitude, self.latitude]
+        
+        self.position = [self.longitude, self.latitude]
 
-        self.gyro_yaw = 0
-        self.encoder_counts0 = 0
+        self.gyroYaw = 0
+        self.encoderCounts0 = 0
 
-        self.prev_bind = [self.longitude, self.latitude]
+        self.prevBind = [self.longitude, self.latitude]
+        self.map = map_maker.get_map()
 
     def _update(self):
-        accel_x, accel_y, accel_z, \
-            self.gyro_yaw, gyro_pitch, gyro_roll = get_imu()
-        self.encoder_counts1 = get_encoder()
-        self.longitude, self.latitude, gps_speed, \
-            gps_angle, satellites, fix_quality = get_gps()
+        # accel_x, accel_y, accel_z, \
+        #     self.gyroYaw, gyro_pitch, gyro_roll = get_imu()
+        # self.encoderCounts1 = get_encoder()
+        # self.longitude, self.latitude, gps_speed, \
+        #     gps_angle, satellites, fix_quality = get_gps()
+        #
+        # self.position[0] += math.cos(self.gyroYaw) * (
+        #     self.encoderCounts1 - self.encoderCounts0)
+        # self.position[1] += math.sin(self.gyroYaw) * (
+        #     self.encoderCounts1 - self.encoderCounts0)
+        # self.encoderCounts0 = self.encoderCounts1
 
-        self.encoder_position[0] += math.cos(self.gyro_yaw) * (
-            self.encoder_counts1 - self.encoder_counts0)
-        self.encoder_position[1] += math.sin(self.gyro_yaw) * (
-            self.encoder_counts1 - self.encoder_counts0)
-        self.encoder_counts0 = self.encoder_counts1
+        # If encoder calculated position is outside of gps error (2 meters?)
+        # reset it to gps values
+
+        pass
 
     def bind(self):
         self._update()
 
-        return # current bind
+        return self.prevBind
+    
--- ben_projects/Self-Driving Buggy Rev. 6/map/map_maker.py
+++ ben_projects/Self-Driving Buggy Rev. 6/map/map_maker.py
@@ -1,64 +1,27 @@
+import csv
+import time
 
-'''
-This module is a helper to the astar module. It works in the /maps directory and
-allows astar to save and recall maps.
+current_file = "Tue Oct 13 20;43;26 2015"
 
-Dependencies:
-- numpy: http://www.numpy.org
-'''
 
-import time # strftime key guide: strftime.org
-import os
-import numpy
+def get_map():
+    with open("storage/" + current_file + ".csv", 'rb') as csvfile:
+        map_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        parsed = [[int(row[0]), int(row[1])] for row in map_reader]
+        return parsed
 
-projectDir = os.path.dirname(os.path.realpath(__file__))
 
-def write(grid, start=(0, 0), end=(0, 0)):
-    '''
-    Write a map text file to the /maps directory.
-    If a map were 3x3, start = (0, 0), and end = (2, 2), the output file would
-    look like:
-        [[0, 0, 1],
-         [0, 0, 1],
-         [1, 1, 1]]
-        (0, 0)
-        (2, 2)
+def write_map(data, file_name=None):
+    if file_name == None:
+        file_name = time.strftime("%c").replace(":", ";")
+    with open("storage/" + file_name + ".csv", 'wb') as csvfile:
+        map_writer = csv.writer(csvfile, delimiter=',',
+                                quotechar='|',
+                                quoting=csv.QUOTE_MINIMAL)
+        for row in data:
+            assert len(row) == 2
+            map_writer.writerow(row)
 
-    :param grid: a 2D numpy array
-    :param start:  a tuple containing the starting (x, y) coordinate
-    :param end:  a tuple containing the end (x, y) coordinate
-    :return: None
-    '''
-    fileName = time.strftime("A* map %b %-d, %Y, %-I-%M%p %Ss.txt", time.localtime(time.time()))
-    print "wrote: " + str(fileName)
-    with open(projectDir + "/Maps/" + fileName, "w") as mapFile:
-        mapFile.write(str(grid.tolist()))
-        mapFile.write("\n" + str(start))
-        mapFile.write("\n" + str(end))
 
-def read():
-    '''
-    Read the first file in the /Maps directory sorted alphabetically.
-
-    :return: a tuple containg a 2D numpy array containing the grid info,
-            start coordinate, and end coordinate
-    '''
-    filePath = projectDir + "/Maps"
-    files = os.listdir(filePath)
-    fileName = ""
-    for file in sorted(files):
-        if "A* map" in file:
-            fileName = file
-            break
-    with open(filePath + fileName, "r") as mapFile:
-        mapString, start, end = mapFile.read().split("\n")
-        start = eval(start)
-        end = eval(end)
-        return numpy.array(eval(mapString), dtype=numpy.int8), start, end
-
-def make(width, height, blocked=None, heightFunc=None):
-    if blocked is not None:
-        raise NotImplementedError
-    if heightFunc is not None:
-        raise NotImplementedError
-    return numpy.zeros((width, height, 2))
+if __name__ == '__main__':
+    print get_map()
