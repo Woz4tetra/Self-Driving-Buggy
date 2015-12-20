cb0aa9490c950723b2e93e076ac6b20178408d49
Woz4tetra <woz4tetra@gmail.com>
2015-12-19 21:20:41 -0500

    comming camera test AGAIN

---
+++ ben_projects/RoboQuasar1.0/tests/keys_camera_test.py
@@ -0,0 +1,23 @@
+"""
+The minimum code required to run the camera module
+"""
+import sys
+
+sys.path.insert(0, '../')
+
+import config
+from camera import capture
+
+
+def run():
+    camera1 = capture.Capture(window_name="camera", camera_type="ps3eye")
+    while True:
+        frame1 = camera1.updateFrame()
+        camera1.showFrame(frame1)
+        key = camera1.getPressedKey()
+        if key > -1:
+            print key
+
+
+if __name__ == '__main__':
+    run()
