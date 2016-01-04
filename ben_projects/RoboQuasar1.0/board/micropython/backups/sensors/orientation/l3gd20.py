42f1a3f607c643a294fb839c5359fa9ed53f4735
emilligan <elyce.milligan@gmail.com>
2016-01-04 16:24:10 -0500

    hi

--- ben_projects/RoboQuasar1.0/board/micropython/backups/sensors/orientation/l3gd20.py
+++
@@ -1,9 +0,0 @@
-
-from .dof_sensor import DOFsensor
-
-class Gyroscope(DOFsensor):
-    L3GD20_ADDRESS = 0
-    def __init__(self, bus):
-        super().__init__(bus, self.L3GD20_ADDRESS)
-
-        self.v_x, self.v_y, self.v_z = 0, 0, 0
